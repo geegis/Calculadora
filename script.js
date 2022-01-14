e7ba06d352126ecc2f9cef76ddcfc999da89ee08
Geovani Sales <salesgeovani@gmail.com>
2022-01-13 23:37:30 -0300

    Mudança do campo resultado para input

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function insert(num){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado + num;
+    var resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado + num;
 }
 
 function limpar(){
-    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('resultado').value = "";
 }
 
 function apagar(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+    var resultado = document.getElementById('resultado').value;
+    document.getElementById('resultado').value = resultado.substring(0, resultado.length -1)
 }
 
 function calcular(){
-    var resultado = document.getElementById('resultado').innerHTML;
+    var resultado = document.getElementById('resultado').value;
     if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado);
+        document.getElementById('resultado').value = eval(resultado);
         document.getElementById('resultado').style.backgroundColor = 'white';
     }else{
         alert("error");
